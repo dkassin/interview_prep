8993987e94708eca266ba831fb67c9432cec5933
David Kassin <davidkassin@Davids-MacBook-Pro.local>
2025-02-05 19:59:40 -0700

    Add all leetcode problems for chapt 5

--- chap_5_arrays/best_time_to_buy_sell_stock.py
+++ chap_5_arrays/best_time_to_buy_sell_stock.py
@@ -1,2 +1,9 @@
 def max_profit(prices) -> int:
-    
+    profit = 0
+    min_val = float(inf)
+    for price in prices:
+        if price < min_val:
+            min_val = price
+            profit = max(profit, price - min_val)
+
+    return profit
